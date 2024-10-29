1c575cd9a01c5265079921bf9a51151b55af4ff9
Alexander Kopylov <sach-egorievsk@yandex.ru>
2024-10-29 15:13:29 +0300

    intM5_vilage

--- InternetMagazine23102024/src/Basket.java
+++ InternetMagazine23102024/src/Basket.java
@@ -1,17 +1,43 @@
 public class Basket {
     private String items = "";
     private int totalPrice = 0;
+    private int limit;
 
-    public static void main(String[] args) {
-
+    //TODO: конструктор нужен для инциализации переменных b
+    public Basket() {
+        limit = 100000;
     }
 
+    public Basket(int limit) {
+        this(); //TODO: вызываем конструктор пустой Basket()
+        this.limit = limit;
+    }
+
+    public Basket(String items, int totalPrice) {
+        this();
+        this.items = this.items + items;
+        this.totalPrice = totalPrice;
+    }
+
+    //TODO: перегрузка свойство создавать одноименные методы с разной сигнатурой
     public void add(String name, int price) {
+        add(name, price, 1);
+    }
+
+    public void add(String name, int price, int count) {
         if (contains(name)) {
             return;
         }
-        items = items + "\n" + name + " - " + price;
-        totalPrice = totalPrice + price;
+        if (totalPrice + price >= limit) {
+            return;
+        }
+        items = items + "\n" + name + " - " +
+                count + " шт. - "+ price;
+        totalPrice = totalPrice + count * price;
+    }
+
+    public void add(int count, String name) {
+        add(name,500, count);
     }
 
     public void clear() {
--- InternetMagazine23102024/src/Main.java
+++ InternetMagazine23102024/src/Main.java
@@ -5,13 +5,17 @@ public class Main {
         vasyaBasket.add("Молоко", 80);
         vasyaBasket.add("Хлеб", 40);
 
-        Basket petyaBasket = new Basket();
+        Basket petyaBasket = new Basket(500);
         petyaBasket.add("Лопата", 280);
         petyaBasket.add("Бочка", 2900);
+
+        Basket mashaBasket = new Basket("Стол",5000);
 
         vasyaBasket.print("Корзина Васи:");
         System.out.println();
         petyaBasket.print("Корзина Пети:");
+        System.out.println();
+        mashaBasket.print("Корзина Маши:");
 
     }
 }
