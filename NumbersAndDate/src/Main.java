b99dac889ba76835ea6abd5c5f8ff39009de8c1b
Alexander Kopylov <sach-egorievsk@yandex.ru>
2024-11-27 14:56:21 +0300

    N&D_8.7

--- NumbersAndDate/src/Main.java
+++ NumbersAndDate/src/Main.java
@@ -1,3 +1,14 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.Locale;
+
 public class Main {
     public static final double EPSILON = 0.000000001;
 
@@ -76,6 +87,76 @@ public class Main {
         double value14 = 100000000000000100.0;
         System.out.println(value13);
         System.out.println(value14);
+
+        double value15 = 0;
+        for (int i = 1; i < 1000000; i++) {
+            value15 += 0.1;
+        }
+        System.out.println(value15);
+        value15 = Math.round(value15);
+        System.out.println(value15);
+
+        //КЛАССЫ ДЛЯ ТОЧНЫХ ВЫЧИСЛЕНИЙ
+
+        BigInteger integer = new BigInteger("6478364823654354334134356486238479");
+        BigInteger integer1 = new BigInteger("64783648236486238479");
+        //integer = integer.remainder(integer1);
+        BigInteger integer2 = new BigInteger("6478364823654354334134356486237536");
+        System.out.println(integer.pow(1000));
+
+        double value16 = 2.438496;
+        BigDecimal decimal = new BigDecimal("2.438496"); //значение необходимо передавать строкой
+        BigDecimal decimal1 = new BigDecimal(value16); //
+        System.out.println("Значение необходимо передавать строкой: " + decimal);
+        System.out.println("Иначе будет неточное число: " + decimal1);
+
+        BigDecimal decimal2 = new BigDecimal("5434132132141.54156415613013");
+        BigDecimal decimal3 = new BigDecimal(3);
+        System.out.println(decimal2.pow(1000));
+
+        //ДАТА И ВРЕМЯ
+        Date now1 = new Date();
+        System.out.println(now1);
+
+        LocalDate todate = LocalDate.now();
+        System.out.println(todate);
+        LocalDateTime currentTime = LocalDateTime.now();
+        System.out.println(currentTime);
+
+        LocalDate birthDate = LocalDate.of(1987, 4, 25);
+        System.out.println(birthDate);
+        System.out.println("Этому человеку исполнится 18 лет в " + birthDate.plusYears(18));
+
+        LocalDateTime nowNY = LocalDateTime.now(ZoneId.of("America/New_York"));
+        System.out.println("Curent time in New York: " + nowNY);
+
+        //парсинг строки
+        String date = "23/01/2021";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        DateTimeFormatter formatter1 = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).localizedBy(new Locale("us"));
+        //LocalDate localDate = LocalDate.parse(date,formatter);
+        LocalDateTime now2 = LocalDateTime.now();
+        //System.out.println("Отражение даты с парсингом " + localDate);
+        System.out.println(formatter.format(now2));
+        System.out.println(formatter1.format(now2));
+
+        //сравнение дат
+        LocalDateTime time1 = LocalDateTime.now();
+        LocalDateTime time2 = LocalDateTime.now().minusDays(2);
+        if (time1.isAfter(time2)) {
+            System.out.println("time1 is after time2");
+        }
+        if (time1.isEqual(time2)) {
+            System.out.println("time1 is after time2");
+        }
+        if (time1.compareTo(time2) > 0) {
+            System.out.println("time1 is after time2");
+        }
+
+        //разница дат
+        System.out.println(time1.until(time2, ChronoUnit.HOURS));
+
+
 
 
 
