eefc0e21f0e5fe3c65b6ed1ceb7618235cd6dcb9
Alexander Copylov <120942867+egorievsk@users.noreply.github.com>
2025-01-17 14:55:53 +0300

    Delete GeometryCalculator directory

--- GeometryCalculator/src/main/java/practice/Main.java
+++
@@ -1,9 +0,0 @@
-package practice;
-
-public class Main {
-    public static final int param = 5;
-
-    public static void main(String[] args) {
-        GeometryCalculator g = new GeometryCalculator();
-    }
-}
