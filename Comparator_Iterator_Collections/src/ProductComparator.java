dc61a4d7f9cf1d8df4da65944ed8787ea5230276
Alexandr Kopylov <sach-egorievsk@yandex.ru>
2025-02-15 17:40:25 +0300

    Revert "beginning"
    This reverts commit 40a2c78cf5c9add5d59eec26ef10178dee16b433.

--- Comparator_Iterator_Collections/src/ProductComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class ProductComparator implements Comparator<String> {
-
-    @Override
-    public int compare(String o1, String o2) {
-        return -o1.compareTo(o2);
-    }
-}
